8bc648636c996667b599edd2f84c3168076741be
Catalin Miclea <micleait@gmail.com>
2018-12-03 14:43:53 +0100

    firstCommit

---
+++ TestGrandhood/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestGrandhood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var numar1 = 1;
+            var numar2 = 2;
+
+            var total = numar1 + numar2;
+
+            ViewBag.test2 = numar1;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var numar1 = 1;
+            var numar2 = 2;
+
+            ViewBag.testTest = (numar1 + numar2) / 3; 
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+
+
+            return View();
+        }
+
+    }
+}
---
+++ TestGrandhood/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestGrandhood.MvcApplication" Language="C#" %>
